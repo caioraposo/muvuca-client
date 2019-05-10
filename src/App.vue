83b8b4da520db8beab4e548d4cb8564feb47e877
Caio Raposo <caioraposo94@gmail.com>
2019-05-10 18:45:19 -0300

    Add Vue.js application

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		{{ pessoas }}
+		aoskdjaksdj
+	</div>
+</template>
+
+<script>
+	export default {
+		data: {
+			pessoas: 43
+		}	
+	}
+</script>
+
+<style>
+</style>
