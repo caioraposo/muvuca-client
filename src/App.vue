f8aec7613e239d964072e0368b24d1a2d156fa44
Caio Raposo <caioraposo94@gmail.com>
2019-05-15 22:11:13 -0300

    Add get number of people in real-time

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
 	<div id="app">
-		{{ pessoas }}
+		<h1>{{ pessoas }}</h1>
 	</div>
 </template>
 
 <script>
+	const { spawn } = require('child_process');
+	
 
 	export default {
 		data() {
 			return {
-				pessoas: 53
+				pessoas: 0
 			}
-		}	
+		},
+		created () {
+			setInterval(() => {
+				const ls = spawn('ls -l ../tempo_real | wc -l', {
+					shell: true
+				});
+
+				ls.stdout.on('data', (data) => {
+					this.pessoas = data;
+				});
+
+			}, 3000)
+		}
 	}
 </script>
 
 <style>
+	h1 {
+		font-size: 100px;
+	}
 </style>
